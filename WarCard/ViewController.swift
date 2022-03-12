854fb89ddb78454c302a9b3f47cbac17bf41738a
Chavin2543 <rungallday@hotmail.com>
2022-03-12 17:07:30 +0700

    Initial Commit

---
+++ WarCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarCard
+//
+//  Created by chavin Panicharoen on 12/3/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
